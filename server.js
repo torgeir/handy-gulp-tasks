d6d2febf377d6843d07ae93e9c9a8187200edb6a
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-10-02 08:28:44 +0200

    Fix silly whitespace.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var connect     = require('connect'),
 var oneDay = 86400000,
     oneYear = oneDay * 365;
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var c = require('./config');
 
